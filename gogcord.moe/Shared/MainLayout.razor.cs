921b540e61714bddb21da695d3ccaeee67bf3211
BasicallyWiz <shushdasher@gmail.com>
2022-04-22 14:34:56 -0700

    Removed the System.Messaging.dll reference. This has been done to allow functionality on not just Windows.

---
+++ gogcord.moe/Shared/MainLayout.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace gogcord.moe.Shared
+{
+  public partial class MainLayout
+  {
+    void ExpandUserCard()
+    {
+      if (CheckLoggedIn())
+      {
+
+      }
+      else
+      {
+
+      }
+    }
+
+    bool CheckLoggedIn()
+    {
+      return false;
+    }
+  }
+}
